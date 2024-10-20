96db7f84e9650373977d47eb711f531947b6c268
Antti Partinen <antti.partinen@student.lab.fi>
2024-10-20 12:43:35 +0300

    Korjattu image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import Link from "next/link" // Importataan Link-komponentti, jolla voidaan luoda navigointilinkkejä jotka eivät lataa kaikkea sisältöä uudelleen
 import ProductCard from "./components/ProductCard/ProductCard"
-import weatherimg from "../images/weather-app.png" // Tuodaan kuva käyttöön
 import { Suspense } from "react";
 
 export default function Home() {
@@ -10,7 +9,7 @@ export default function Home() {
       <div className="py-5">
         <p className="text-2xl text-center">Weather App</p>
         <Suspense fallback={<div>Loading...</div>}>
-          <Image src={weatherimg} alt={"Weather App"}></Image>
+          <Image src="/images/weather-app.png" width={1920} height={1080} alt={"Weather App"}></Image>
         </Suspense>
         < ProductCard />
       </div>
